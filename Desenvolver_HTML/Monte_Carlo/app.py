2dacdf0523c7eb73eb0fbb4e4f27358edd8d78df
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-08-30 18:19:10 +0100

    Bug resolvido era a 'OPTIONS' tag que não estava especificada

--- Desenvolver_HTML/Monte_Carlo/app.py
+++ Desenvolver_HTML/Monte_Carlo/app.py
@@ -4,7 +4,7 @@ import random
 import math 
 import time
 import logging
-from flask import Flask, request, jsonify, session, Response
+from flask import Flask, request, jsonify, session, Response, render_template
 # from flask_session import Session
 # from flask_csv import send_csv
 from flask_cors import CORS
@@ -18,7 +18,7 @@ frist = 1
 i = 1
 total_in = 0
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='html', static_folder='html/')
 app.secret_key = '*}6Ttt)G7X_T}3VF:ygc'
 
 
@@ -31,13 +31,12 @@ app.debug = False
 #app.logger.disabled = False
 
 
-
 logging.basicConfig(level=logging.DEBUG) # CRITICAL, ERROR, WARNING, INFO, and DEBUG 
 
 log = logging.getLogger('werkzeug')
 log.disabled = True
 
-CORS(app)
+CORS(app,automatic_options=False)
 
 
 def receive_data_from_exp():
@@ -52,21 +51,22 @@ def receive_data_from_exp():
         sleep(0.01)
         print("Pi: %lf"%(float(total_in)*4/float(n_points)))
         return "DATA_END"
-    sleep(0.01)
-    
-    x = random.random()*float(size)
-    y = random.random()*float(size)
-    if math.sqrt(x*x+y*y) <=int(size):
-        c_in = 1
-        total_in = total_in + 1
     else:
-        c_in = 0
-    pic_message =   {"Sample_number":str(i),
-                    "eX":str(x),
-                    "eY":str(y),
-                    "circ":str(c_in)}
-    # print (i)
-    i=i+1
+        sleep(0.01)
+        
+        x = random.random()*float(size)
+        y = random.random()*float(size)
+        if math.sqrt(x*x+y*y) <=int(size):
+            c_in = 1
+            total_in = total_in + 1
+        else:
+            c_in = 0
+        pic_message =   {"Sample_number":str(i),
+                        "eX":str(x),
+                        "eY":str(y),
+                        "circ":str(c_in)}
+        # print (i)
+        i=i+1
     return pic_message
 
 def do_config(config_json) :
@@ -90,36 +90,51 @@ def do_config(config_json) :
     return  config_json,True
 
 def ConfigureStartExperiment(user_json):
-    global i
-    i = 1
+    
     do_config(user_json)
     return ''
 
-@app.route('/user', methods=['POST'])
+@app.route('/',methods=['GET'])
+def HTML_exe():
+    return render_template("index-elab.html")
+
+@app.route('/user', methods=['POST','OPTIONS'])
 def Flask_f1():
+    global i
+    global frist
+    frist =1
+    i = 1
     if request.method == 'POST':
         #origin = request.headers.get('Origin')	
         print(request.data)
         user_json = json.loads(request.data.decode(FORMAT))
         ConfigureStartExperiment(user_json)
         return '' #jsonify({'JSON Enviado' : request.args.get('JSON'), 'result': 'OK!'})
+    elif request.method == 'OPTIONS':
+        return ''
 
 
 
 @app.route('/resultpoint', methods=['GET'])
 def getPoint():
+    global end
     exp_data = receive_data_from_exp()
-    send_data =     {"msg_id" : "11",
-                    "timestamp" : str(time.time_ns()),
-                    "status" : "running", 
-                    "Data" : exp_data}
-    print(send_data)
-    if exp_data == "DATA_END": 
+    send_data = {"msg_id" : "11",
+                 "timestamp" : str(time.time_ns()),
+                 "status" : "waiting", 
+                 "Data" : " "}
+
+    if exp_data == "DATA_END": #and end == 0:
         send_data =     {"msg_id":"11",
                         "timestamp": str(time.time_ns()),
                         "status":"Experiment Ended",
                         "Data":" "}
-   
+    else :
+        send_data =     {"msg_id" : "11",
+                        "timestamp" : str(time.time_ns()),
+                        "status" : "running", 
+                        "Data" : exp_data}
+    print(send_data)
     return send_data
 
 
